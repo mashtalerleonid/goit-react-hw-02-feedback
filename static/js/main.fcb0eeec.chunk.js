96e6e327b24b85bfdea4cdd572fa7d1372bcedae
mashtalerleonid <mashtalerleonid@gmail.com>
2021-08-31 19:35:19 +0300

    Updates

--- static/js/main.fcb0eeec.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{10:function(e,t,c){},4:function(e,t){},5:function(e,t,c){"use strict";c.r(t);var n=c(0),o=c.n(n),a=c(3),i=c.n(a),r=(c(10),c(4)),s=c.n(r),d=c(2);i.a.render(Object(d.jsx)(o.a.StrictMode,{children:Object(d.jsx)(s.a,{})}),document.getElementById("root"))}},[[5,1,2]]]);
-//# sourceMappingURL=main.fcb0eeec.chunk.js.map
